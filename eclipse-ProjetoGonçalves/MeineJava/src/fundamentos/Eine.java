0ef070c7aa0346d96820b61fc14fd236cbc6f870
projetos2 <projetos2@3CTA-PRJ02>
2026-08-07 11:42:28 -0300

    new commit

--- eclipse-ProjetoGonçalves/MeineJava/src/fundamentos/Eine.java
+++ eclipse-ProjetoGonçalves/MeineJava/src/fundamentos/Eine.java
@@ -3,5 +3,11 @@ package fundamentos;
 public class Eine {
 	public static void main(String[] args) {
 		System.out.println("Guten Tag");
+		System.out.println("Wie geht´s dir?");
+
+//		{
+//			// pode ser criado novo bloco de código
+//			// mas não tem motivo para fazer isso
+//		}
 	}
 }
