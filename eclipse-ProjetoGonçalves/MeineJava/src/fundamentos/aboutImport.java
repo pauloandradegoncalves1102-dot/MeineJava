b599dc6195f4890fa013ed5a8876efb39b6ac9f0
Paulo Andrade <pauloandradegoncalves1102@gmail.com>
2026-08-13 14:27:09 -0300

    Commit about Import in Java

---
+++ eclipse-ProjetoGonçalves/MeineJava/src/fundamentos/aboutImport.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+import java.util.Date; // importar Date
+
+import javax.swing.JButton;
+
+
+
+public class aboutImport {
+
+	public static void main(String[] args) {
+		
+		java.lang.String b = "Bom dia!";
+		System.out.println(b);
+		
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		
+	}
+}
