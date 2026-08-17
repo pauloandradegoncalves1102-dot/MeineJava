e31e9f43113056abc892088bff105ae31b3a44bc
Paulo Andrade <pauloandradegoncalves1102@gmail.com>
2026-08-17 12:40:12 -0300

    NEW COMMIT TODAY

---
+++ eclipse-ProjetoGonçalves/MeineJava/src/fundamentos/conversaoTiposPrimitivos.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+public class conversaoTiposPrimitivos {
+	 public static void main(String[] args) {
+			
+		 double a = 1; // conversão implicita
+		 System.out.println(a);
+		 
+		 float b = 1.0F; // float b = (Float) 1.0; - explicita
+		 System.out.println(b);
+		 
+		 int c = 4;
+		 byte d =(byte) c;
+		 System.out.println(d);
+		 
+		 double e = 1;
+		 int f = (int) e;
+		 System.out.println(f);
+		 
+		 
+		 
+	}
+
+}
